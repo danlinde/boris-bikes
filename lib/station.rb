459d584ed5d4e547a8e83c9e71279d11daceae2e
mindfulcoder <jodie@mindfulcoder.com>
2013-09-02 20:28:20 +0100

    Added the ability to rent a bike and update sation bike list

--- lib/station.rb
+++ lib/station.rb
@@ -5,11 +5,6 @@ class Station
 		@list_of_bikes = list_of_bikes
 	end
 
-	def number_of_bikes_available
-		number_of_bikes_available = @list_of_bikes.select {|serial, availability| availability == 'Available'}
-		number_of_bikes_available.length
-	end
-
 	def name
     return @name
 	end
@@ -18,8 +13,31 @@ class Station
     return @list_of_bikes
   end  
 
-	def rent_a_bike?
-	   true
+  def bike_available?
+    @num_bikes_available = @list_of_bikes.select {|serial, availability| availability == 'Available'}
+		@num_bikes_available.length > 0
+  end
+
+	def rent_out_a_bike
+    if bike_available?
+    	 @serial_number = serial_num
+       remove_bike_from_list(@serial_num)
+       @serial_num
+    end
+  end
+
+  def serial_num
+     @bikes = @list_of_bikes.select {|serial, availability| availability == 'Available'}
+	   @serial_num = @bikes.first[0]
+	   @serial_num
+  end
+	
+	def remove_bike_from_list(serial_num)
+    @list_of_bikes.delete(serial_num)
+	end
+
+	def bike_unavailable(serial_num)
+     @list_of_bikes[serial_num] = "Not Available"
 	end
 
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,19 +2,31 @@ require_relative '../lib/station'
 
 describe Station do 
   let(:station){Station.new('Pimlico', {'s-123-ab' => 'Available', 's-234-cd' => 'Not Available'})}
+  
   it 'can referenced by name' do 
   	expect(station.name).to eq 'Pimlico'
   end
 
-	it 'returns the number bikes available' do
-	  expect(station.number_of_bikes_available()).to eq 1
-	end
-
-  it 'can rent a bike' do 
-    expect(station.rent_a_bike?()).to be_true
+  it 'can create a list of bike details' do 
+    expect(station.list_of_bikes()).to eq({"s-123-ab"=>"Available", "s-234-cd"=>"Not Available"})
   end
 
-  it 'can create a list of bike details' do 
-  	expect(station.list_of_bikes()).to eq({"s-123-ab"=>"Available", "s-234-cd"=>"Not Available"})
+  context 'when bikes available at station' do
+
+    it 'returns the serial number of the bike rented' do 
+      expect(station.rent_out_a_bike).to eq 's-123-ab'
+    end
+
+    it 'can update a bike to not available' do 
+      expect(station.bike_unavailable("s-123-ab")).to eq "Not Available"
+    end
+
+    it 'can tell you if bikes are available' do 
+      expect(station.bike_available?).to eq true
+    end
+
+    it "returns an available bike's serial number" do 
+      expect(station.serial_num).to eq 's-123-ab'
+    end
   end
 end
