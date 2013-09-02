e594088b60fbae51bbb533634679c11f49e62868
mindfulcoder <jodie@mindfulcoder.com>
2013-09-02 17:01:20 +0100

    refactored tests to utilize double method and method stub for station

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,30 +2,37 @@ require_relative '../lib/user'
 
 describe User do
 	let(:user) {User.new}
+	let(:station) do 
+	  double :station, 
+	  'has_bikes?' => true, 
+	  'has_no_bikes?' => false,
+	  'has_space?' => true,
+	  'has_no_space?' => false
+	end 
+
   context 'when bikes available at station' do 
-  	station_has_bikes = true
 	  it "can rent a bike" do 
-	  	expect(user.rents_a_bike?(station_has_bikes)).to be_true
+	  	expect(user.rents_a_bike?(station.has_bikes?)).to be_true
 	  end  
 	end
   context 'when bikes unavailable at station' do 
   	station_has_bikes = false
 	  it "cannot rent a bike" do 
-	  	expect(user.rents_a_bike?(station_has_bikes)).to be_false
+	  	expect(user.rents_a_bike?(station.has_no_bikes?)).to be_false
 	  end
 	end
 
 	context "when space available at station" do 
 		station_space_available = true
 		it "can return bike" do 
-			expect(user.returns_a_bike?(station_space_available)).to be_true
+			expect(user.returns_a_bike?(station.has_space?)).to be_true
 		end
 	end
 
 	context 'when space unavailable at station' do 
 		station_space_available = false 
 		it "cannot return bike" do 
-			expect(user.returns_a_bike?(station_space_available)).to be_false
+			expect(user.returns_a_bike?(station.has_no_space?)).to be_false
 		end
 	end
 end
