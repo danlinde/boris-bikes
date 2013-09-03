47b5c9eff693859ef4e77bb6f5a8414e55b9333c
mindfulcoder <jodie@mindfulcoder.com>
2013-09-03 19:01:06 +0100

    add to station class - capacity, load and unload van. Refactored existing methods

--- lib/station.rb
+++ lib/station.rb
@@ -1,8 +1,12 @@
+
 class Station
 
-	def initialize(name,list_of_bikes)
+  attr_reader :serial_num, :list_of_bikes, :bike_available
+
+	def initialize(name,list_of_bikes, capacity)
 		@name = name
 		@list_of_bikes = list_of_bikes
+		@capacity = capacity
 	end
 
 	def name
@@ -13,31 +17,54 @@ class Station
     return @list_of_bikes
   end  
 
-  def bike_available?
-    @num_bikes_available = @list_of_bikes.select {|serial, availability| availability == 'Available'}
+  def bike_available
+    @num_bikes_available = @list_of_bikes.select {|serial, availability| availability == 'available'}
 		@num_bikes_available.length > 0
   end
 
 	def rent_out_a_bike
-    if bike_available?
-    	 @serial_number = serial_num
-       remove_bike_from_list(@serial_num)
-       @serial_num
+		if bike_available
+  	  @bike = self.bike  # this returned number needs to be stored somewhere
+      remove_bike_from_list(@bike)
+      @bike
     end
   end
 
-  def serial_num
-     @bikes = @list_of_bikes.select {|serial, availability| availability == 'Available'}
-	   @serial_num = @bikes.first[0]
-	   @serial_num
+  def bike
+     @bikes = @list_of_bikes.select {|serial, availability| availability == 'available'}
+	   @bike = @bikes.first[0]
+	   @bike
   end
 	
 	def remove_bike_from_list(serial_num)
-    @list_of_bikes.delete(serial_num)
+    @list_of_bikes.delete(bike)
 	end
 
-	def bike_unavailable(serial_num)
-     @list_of_bikes[serial_num] = "Not Available"
+	def broken(bike)
+     @list_of_bikes[bike] = "broken"
 	end
 
+	def space_available
+		(@capacity - @list_of_bikes.length) > 0
+	end
+
+	def accept_bike(bike)
+		@list_of_bikes.merge!(bike) if space_available
+	end
+
+	def broken_bikes
+		@list_of_bikes.select {|serial, availability| availability == 'broken'}
+	end
+
+	def load_van(van)
+		van.accept_broken_bikes(broken_bikes)
+    @list_of_bikes.each { |key, value| @list_of_bikes[key] = 'garage' if value == 'broken'}
+  end
+
+  def unload_van(van)
+  	van.deliver_fixed_bikes
+    @list_of_bikes = @list_of_bikes.each {|key, value| @list_of_bikes[key] = 'available' if value == 'garage'}
+   end
 end
+
+
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,32 +1,72 @@
 require_relative '../lib/station'
 
 describe Station do 
-  let(:station){Station.new('Pimlico', {'s-123-ab' => 'Available', 's-234-cd' => 'Not Available'})}
-  
+  let(:station){Station.new('Pimlico', {'s-123-ab' => 'available', 's-234-cd' => 'broken'}, 10)}
+  let(:station_garage){Station.new('Pimlico', {'s-123-ab' => 'available', 's-234-cd' => 'broken', 's-235-cd' => 'garage'}, 10)}
   it 'can referenced by name' do 
   	expect(station.name).to eq 'Pimlico'
   end
 
   it 'can create a list of bike details' do 
-    expect(station.list_of_bikes()).to eq({"s-123-ab"=>"Available", "s-234-cd"=>"Not Available"})
+    expect(station.list_of_bikes()).to eq({"s-123-ab"=>"available", "s-234-cd"=>"broken"})
   end
 
-  context 'when bikes available at station' do
-
-    it 'returns the serial number of the bike rented' do 
+    it 'gives the bike rented if bikes available' do 
       expect(station.rent_out_a_bike).to eq 's-123-ab'
     end
 
-    it 'can update a bike to not available' do 
-      expect(station.bike_unavailable("s-123-ab")).to eq "Not Available"
+    it 'can update a bike to broken' do 
+      expect(station.broken("s-123-ab")).to eq "broken"
     end
 
     it 'can tell you if bikes are available' do 
-      expect(station.bike_available?).to eq true
+      expect(station.bike_available).to eq true
     end
 
-    it "returns an available bike's serial number" do 
-      expect(station.serial_num).to eq 's-123-ab'
+    it 'returns an available bike' do 
+      expect(station.bike).to eq 's-123-ab'
     end
-  end
+
+    it 'can tell you if space is available' do 
+      expect(station.space_available).to eq true
+    end
+
+    it 'can accept a bike if space available' do 
+      expect(station.accept_bike('s-222-ab' => 'working')).to eq({"s-123-ab"=>"available", "s-234-cd"=>"broken", "s-222-ab"=>"working"})
+    end
+
+    it 'knows about all broken bikes' do
+      expect(station.broken_bikes).to eq({'s-234-cd' => 'broken'})
+    end 
+    
+    it 'asks a van to accept broken bikes' do
+      van = double :van 
+      expect(van).to receive(:accept_broken_bikes)
+
+      station.load_van(van)
+    end
+
+    it 'updates list of bikes to show bikes sent to garage' do
+      van = double(:van, {:accept_broken_bikes => true})
+      station.load_van(van)
+      expect(station.list_of_bikes).to eq({'s-123-ab' => 'available', 's-234-cd' => 'garage'})
+    end
+
+    it 'asks a van to return fixed bikes' do 
+      van = double :van 
+      expect(van).to receive(:deliver_fixed_bikes)
+
+      station.unload_van(van)
+    end
+
+    it 'updates list of bikes to show bikes sent to garage' do
+      
+      van = double(:van, {:deliver_fixed_bikes => true})
+      station_garage.unload_van(van)
+      expect(station_garage.list_of_bikes).to eq({"s-123-ab"=>"available", "s-234-cd"=>"broken", "s-235-cd"=>"available"})
+    end
+
+
+
+
 end
